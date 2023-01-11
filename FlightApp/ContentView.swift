9fedfd5fd5d7a64f55b2d73ee2601c2d7988484a
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-01-10 20:01:25 -0500

    Initial Commit

---
+++ FlightApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FlightApp
+//
+//  Created by Sergey Lukaschuk on 2023-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlightApp/FlightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppApp.swift
+//  FlightApp
+//
+//  Created by Sergey Lukaschuk on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
